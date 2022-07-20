85e3272b3b9ccf7e0f569530e63dcaefd4ce7531
sora <ytgbjdrncugtc@knowledgemd.com>
2022-07-19 21:07:11 -0500

    General

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 
+let selectionButtons = document.querySelectorAll('[data-selection]');
+
+selectionButtons.forEach(selectionButtons => {
+    selectionButtons.addEventListener('click', e => {
+    const selectionName = selectionButtons.dataset.selection
+    })
+})
