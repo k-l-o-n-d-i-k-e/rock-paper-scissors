bcd96734125e4ce38ab5fc75ecd83c5e90e12f8e
sora <ytgbjdrncugtc@knowledgemd.com>
2022-07-20 08:57:47 -0500

    Rock-paper-scissors is now playable

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
-let selectionButtons = document.querySelectorAll('[data-selection]');
-const finalColumn = document.querySelectorAll('[data-final-column]');
+const selectionButtons = document.querySelectorAll('[data-selection]');
+const finalColumn = document.querySelector('[data-final-column]');
 
 /*this helps organize the different number of selections with an array. It converts the
 emojis into text*/
@@ -22,14 +22,11 @@ const SELECTIONS = [
     }
 ]
 //still don't know exactly what this does.
-    selectionButtons.forEach(selectionButtons => {
-
+selectionButtons.forEach(selectionButtons => {
     selectionButtons.addEventListener('click', e => {
-
     //it seems that with this line of code you can search for the data selection 'class' names.
     const selectionName = selectionButtons.dataset.selection
-
-    const selection = SELECTIONS.find(selection => selection.name == selectionName)
+    const selection = SELECTIONS.find(selection => selection.name === selectionName)
     makeSelection(selection)
     })
 })
@@ -39,21 +36,15 @@ function makeSelection(selection) {
     const computerSelection = randomSelection()
     const yourWinner = isWinner(selection, computerSelection)
     const computerWinner = isWinner(computerSelection, selection)
-    console.log(computerSelection)
-
     addSelectionResult(computerSelection, computerWinner)
     addSelectionResult(selection, yourWinner)
 }
 
 function addSelectionResult(selection, winner){
-
     const div = document.createElement('div')
     div.innerText = selection.emoji
     div.classList.add('result-selection')
-    
-    if (winner){
-        div.classList.add('winner')
-    }
+    if (winner) div.classList.add('winner')
     finalColumn.after(div)
 }
 
